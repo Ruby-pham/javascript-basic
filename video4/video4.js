c5addb992dc935d685e813b20ab83aa62f1f7e1b
javascript-ngoc <rubypham955@gmail.com>
2023-11-21 16:20:44 +0700

    chater 2 video4

---
+++ video4/video4.js
@@ -0,0 +1,8 @@
+let a = 'I am a string - text';
+let text = `We are the so-called 
+
+"Vikings" from the north.`;
+console.log(a)
+console.log('a' + 4 + 5)
+console.log(4 + 5 + 'a')
+console.log(text)
