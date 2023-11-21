237b0ae01046e729d69748b5d200457a8e563e62
javascript-ngoc <rubypham955@gmail.com>
2023-11-21 17:44:54 +0700

    chater 2 video 6

---
+++ video6/video6.js
@@ -0,0 +1,15 @@
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5; //int, float, long, double
+let b = 6;
+let c = 6.9;
+let d = '5'
+let e = Number(d);
+let f = +d;
+console.log('f value: ', f, 'type: ', typeof f)
+console.log('e value: ', e, 'type: ', typeof e)
+console.log(a + b)
+console.log(a + d)
+console.log(a * d)
+console.log(a - d)
+console.log(a === d)
+console.log(a === d, 'a:', typeof a, 'd:', typeof d)
