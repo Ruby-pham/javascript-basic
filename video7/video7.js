bb7c05f00bc91d84487701b3b08a51ffb3eab548
javascript-ngoc <rubypham955@gmail.com>
2023-11-23 03:30:49 +0700

    chapter 2 video7

---
+++ video7/video7.js
@@ -0,0 +1,7 @@
+console.log('Hello world from HTML')
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+console.log(`hoi dan it check string: a = ${a}, b = ${b}, a+b = ${a+b}`)
