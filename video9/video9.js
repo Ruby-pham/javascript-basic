7da6f653d36e0d09e720ed1d8cedf1933eb5c7a7
ruby pham <rubypham955@gmail.com>
2023-11-27 21:57:07 -0500

    chapter 2 video9

---
+++ video9/video9.js
@@ -0,0 +1,10 @@
+// object
+let a = {name: "Eric", age: 25, gender: "male"}
+console.log('Information of a:',a)
+// array = [item1, item2, item3], each item maybe is object or string or number
+let b = [{name: "Eric"}, {age: 25}, {gender: "male"}]
+console.log('type of b:', typeof b, b)
+// let pickup a item
+console.log('what your name', b[0])
+console.log('how old are you', b[1])
+console.log('how is your gender', b[2])
